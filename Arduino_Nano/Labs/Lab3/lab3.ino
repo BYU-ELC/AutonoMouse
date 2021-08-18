c0f067a6483d1a784f68008a5d39ade21b9adffb
Joshua Miller <jmyoshibanks@gmail.com>
2021-08-18 14:26:14 -0600

    Added Pdfs

--- Arduino_Nano/Labs/Lab3/lab3.ino
+++ Arduino_Nano/Labs/Lab3/lab3.ino
@@ -51,33 +51,41 @@ int incomingByte = 0;   // for incoming serial data
 
 void setup() {
   Serial.begin(9600);
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(11);  // attaches the servo on pin 9 to the servo object
+  myservo.write(0);
+  delay(2000);
+  myservo.write(90);
+  delay(500);
+  myservo.write(180);
+  delay(2000);
+  
+  myservo.detach();
 }
 
 
 
 void loop() {
-
-        // send data only when you receive data:
-        if (Serial.available() > 0) {
-                // read the incoming byte:
-                incomingByte = Serial.read();
-
-                // say what you got:
-                Serial.print("received: ");
-                Serial.print (incomingByte);
-                if(incomingByte == 108){
-                 Serial.println(" sent 0 Rotaing CW "); 
-                 myservo.write(0); 
-                }else if(incomingByte == 114){
-                  Serial.println(" sent 180 Rotaing CCW "); 
-                  myservo.write(180); 
-                }else if(incomingByte == 60){
-                  Serial.println(" sent Stopped "); 
-                  myservo.write(60); 
-                }else{
-                  Serial.println(" moving Random"); 
-                  myservo.write(incomingByte); 
-                }        
-        }
+//
+//        // send data only when you receive data:
+//        if (Serial.available() > 0) {
+//                // read the incoming byte:
+//                incomingByte = Serial.read();
+//
+//                // say what you got:
+//                Serial.print("received: ");
+//                Serial.print (incomingByte);
+//                if(incomingByte == 108){
+//                 Serial.println(" sent 0 Rotaing CW "); 
+//                 myservo.write(0); 
+//                }else if(incomingByte == 114){
+//                  Serial.println(" sent 180 Rotaing CCW "); 
+//                  myservo.write(180); 
+//                }else if(incomingByte == 60){
+//                  Serial.println(" sent Stopped "); 
+//                  myservo.write(60); 
+//                }else{
+//                  Serial.println(" moving Random"); 
+//                  myservo.write(incomingByte); 
+//                }        
+//        }
 } 
--- README.md
+++ README.md
@@ -9,3 +9,15 @@ V2's objective is to build a similiar autonomous car using ESP32 as a controller
 allow's the user to put the car into remote control mode where the user can control the car via bluetooth
 using an Android smartphone. This doesn't work with an IPhone because there are not opensource bluetooth apps 
 that I know of in the App Store. 
+
+# Things to Change
+Fix the circuit. When Arduino is plugged into the computer placed inside the pcb the AMS1117 voltage regulator on the Arduino gets really hot.
+It's because the VIN Pin has a direct path to ground through the switch so it's passing a lot of current and gets hot to let off the energy converted. 
+Is there way to fix this with a diode? I'm not the best with circuits.
+Right now I have to take out the Nano to program it, place it back in, and then power it with the battery. The switch is flipped with the battery activated so there's not the problem with the voltage regulator. 
+
+More accessibility for linux and Apple users. Document how to download Arduino IDE and how to interfaces with Arduino devices.
+
+Finish development for V2. 
+
+
